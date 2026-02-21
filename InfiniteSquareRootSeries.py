55cd8973b7f1a63bea497232195cc7f08821497b
Fr33styler <Fr33stylerMC@gmail.com>
2026-02-21 11:05:56 +0200

    Added square root infinite series
    Added square root infinite series

---
+++ InfiniteSquareRootSeries.py
@@ -0,0 +1,10 @@
+#Applying the infinite series of the square root for any radicand r (r ∈ R, r ∈ C)
+
+s = 0
+r = input("Specify radicand: ")
+r = complex(r) if "j" in r else float(r)
+
+for _ in range(int(input("Specify range: "))):
+  s = (r - 1) / (2 + s)
+
+print(1 + s)
